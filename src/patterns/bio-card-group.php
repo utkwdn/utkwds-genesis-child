d26d5fea077b321c39ec81abdb7f46af52c1caaf
Sandra Paduch <69162526+spworky@users.noreply.github.com>
2022-11-16 11:05:49 -0500

    bio card – make generic, swap out image to non-local image, add description of how to use pattern, remove chancellor cut corner

--- src/patterns/bio-card-group.php
+++ src/patterns/bio-card-group.php
@@ -12,123 +12,108 @@
 <div class="wp-block-group bioCardContainer"><!-- wp:group {"className":"bioCard"} -->
 <div class="wp-block-group bioCard"><!-- wp:group {"className":"bioHeadshot"} -->
 <div class="wp-block-group bioHeadshot"><!-- wp:image {"id":4870,"sizeSlug":"large","linkDestination":"none"} -->
-<figure class="wp-block-image size-large"><img src="https://images.utk.edu/wds/headshot-placeholder-1480x1920.png" alt="Headshot Placeholder 1480x1920" class="wp-image-4870"/></figure>
+<figure class="wp-block-image size-large"><img src="https://images.utk.edu/wds/headshot-placeholder-740x960.png" alt="Headshot Placeholder 740x960" class="wp-image-4870"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"className":"bioInfo"} -->
 <div class="wp-block-group bioInfo"><!-- wp:heading {"level":3} -->
-<h3>Joy Harjo</h3>
+<h3>Bio Card Group</h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><em>Honorary Doctor of Humanities, Spring 2022</em>
-</p>
+<p><em>Basic Structure (parent group – card group – [image group, content group]</em></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p>Joy Harjo, the 23rd poet laureate of the United States, is an internationally renowned performer and writer of the Muscogee (Creek) Nation. Harjo is the author of nine books of poetry, several plays, children’s books, and two memoirs; she has also edited several anthologies and produced seven award-winning music albums. Her many honors include the Ruth Lily Prize from the Poetry Foundation, the Academy of American Poets Wallace Stevens Award, two NEA fellowships, and a Guggenheim Fellowship. </p>
+<p>This pattern uses nested flex groups. To add more cards, just duplicate the card group within the parent group. To add additional content inside each card, add blocks inside the content group inside each card. </p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p>A former John C. Hodges Chair of Excellence at UT, Harjo is currently a chancellor of the Academy of American Poets and chair of the Native Arts and Cultures Foundation Board of Directors. She is a member of the National Women’s Hall of Fame, the National Native American Hall of Fame, the American Philosophical Society, and the American Academy of Arts and Letters. She lives in Tulsa, Oklahoma, where she is the inaugural artist-in-residence of the Bob Dylan Center. </p>
+<p>Recommended headshot site is: 740px x 960px </p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"className":"bioCard"} -->
 <div class="wp-block-group bioCard"><!-- wp:group {"className":"bioHeadshot"} -->
-<div class="wp-block-group bioHeadshot"><!-- wp:image {"id":4945,"sizeSlug":"large","linkDestination":"none"} -->
-<figure class="wp-block-image size-large"><img src="https://images.utk.edu/wds/headshot-placeholder-1480x1920.png" alt="Headshot Placeholder 1480x1920" class="wp-image-4945"/></figure>
+<div class="wp-block-group bioHeadshot"><!-- wp:image {"id":4870,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://images.utk.edu/wds/headshot-placeholder-740x960.png" alt="Headshot Placeholder 740x960" class="wp-image-4870"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"className":"bioInfo"} -->
 <div class="wp-block-group bioInfo"><!-- wp:heading {"level":3} -->
-<h3>Bill Dance</h3>
+<h3>First Last</h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><em>Honorary Doctor of Natural Resources, Spring 2021</em>
-</p>
+<p><em>Honorary Doctor of Specific Things, Spring 2022</em> </p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p>As a professional fisher, television personality, author, conservationist, teacher, and native Tennessean, Bill Dance is one of the most recognizable outdoor sports figures in the world. Dance—whose trademark for 40 years has been an orange-and-white UT baseball cap—quickly gained notoriety and success during his time on the professional bass fishing tournament trail in the 1970s. His notable accomplishments include three-time B.A.S.S. Angler of the Year, eight-time Bassmaster Classic Qualifier, seven tournament wins, eight runners up, six third place, and 40 top 10 finishes. However, most people know him through his television show <em>Bill Dance Outdoors</em>, which is one of the longest-running television shows in history with well over 2,000 episodes. Dance is also the author of seven books and has been a regular contributor to national magazines. </p>
+<p>Ut enim blandit volutpat maecenas volutpat blandit aliquam. Neque sodales ut etiam sit amet. Nunc lobortis mattis aliquam faucibus purus in massa tempor. Nisi lacus sed viverra tellus in hac habitasse platea. Eget aliquet nibh praesent tristique magna sit amet. Ante in nibh mauris cursus mattis. Egestas diam in arcu cursus euismod quis viverra nibh. Cras sed felis eget velit aliquet sagittis. Bibendum arcu vitae elementum curabitur vitae nunc sed velit dignissim. Nibh venenatis cras sed felis eget velit aliquet. Maecenas accumsan lacus vel facilisis volutpat. Egestas maecenas pharetra convallis posuere morbi. Vitae justo eget magna fermentum iaculis eu. Sed risus pretium quam vulputate dignissim suspendisse. Volutpat ac tincidunt vitae semper quis lectus nulla at. Facilisi cras fermentum odio eu feugiat pretium nibh ipsum. Tellus rutrum tellus pellentesque eu. Pellentesque sit amet porttitor eget dolor morbi.</p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p>His contributions to outdoor recreation, conservation, and economic development have been recognized across the globe. His notable awards include the Congressional National Water Safety Award; induction into the National Freshwater Hall of Fame; induction into the Professional Bass Fishing Hall of Fame; the President’s Award for the National Association of State Boating Law Administrators; induction into the International Game Fish Association Hall of Fame; and being honored by the Tennessee Sports Hall of Fame as both as an inductee and winner of the Tennessee Male Professional Athlete of the Year award. </p>
+<p>Aliquam sem et tortor consequat id porta nibh. Non diam phasellus vestibulum lorem sed risus ultricies tristique. Id neque aliquam vestibulum morbi blandit cursus risus at. Luctus accumsan tortor posuere ac. Enim ut tellus elementum sagittis vitae et leo duis ut. Orci nulla pellentesque dignissim enim. Augue mauris augue neque gravida in fermentum et sollicitudin. Integer eget aliquet nibh praesent tristique magna sit amet. Risus nec feugiat in fermentum posuere urna. Eget aliquet nibh praesent tristique magna sit amet purus gravida. Consectetur adipiscing elit pellentesque habitant morbi.</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"className":"bioCard"} -->
 <div class="wp-block-group bioCard"><!-- wp:group {"className":"bioHeadshot"} -->
-<div class="wp-block-group bioHeadshot"><!-- wp:image {"id":4871,"sizeSlug":"large","linkDestination":"none"} -->
-<figure class="wp-block-image size-large"><img src="https://images.utk.edu/wds/headshot-placeholder-1480x1920.png" alt="Headshot Placeholder 1480x1920" class="wp-image-4871"/></figure>
+<div class="wp-block-group bioHeadshot"><!-- wp:image {"id":4870,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://images.utk.edu/wds/headshot-placeholder-740x960.png" alt="Headshot Placeholder 740x960" class="wp-image-4870"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"className":"bioInfo"} -->
 <div class="wp-block-group bioInfo"><!-- wp:heading {"level":3} -->
-<h3>Theotis Robinson Jr.</h3>
+<h3>First Last</h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><em>Honorary Doctor of Social Work, Fall 2019</em></p>
+<p><em>Honorary Doctor of Specific Things, Spring 2022</em> </p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p>After opening UT’s doors for African American undergraduates, Theotis Robinson Jr. has spent his lifetime continuing to advance social justice.
-</p>
+<p>Ut enim blandit volutpat maecenas volutpat blandit aliquam. Neque sodales ut etiam sit amet. Nunc lobortis mattis aliquam faucibus purus in massa tempor. Nisi lacus sed viverra tellus in hac habitasse platea. Eget aliquet nibh praesent tristique magna sit amet. Ante in nibh mauris cursus mattis. Egestas diam in arcu cursus euismod quis viverra nibh. Cras sed felis eget velit aliquet sagittis. Bibendum arcu vitae elementum curabitur vitae nunc sed velit dignissim. Nibh venenatis cras sed felis eget velit aliquet. Maecenas accumsan lacus vel facilisis volutpat. Egestas maecenas pharetra convallis posuere morbi. Vitae justo eget magna fermentum iaculis eu. Sed risus pretium quam vulputate dignissim suspendisse. Volutpat ac tincidunt vitae semper quis lectus nulla at. Facilisi cras fermentum odio eu feugiat pretium nibh ipsum. Tellus rutrum tellus pellentesque eu. Pellentesque sit amet porttitor eget dolor morbi.</p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p>It was Robinson’s persistence in pursuing his application that eventually led to a change in the university’s admissions policy in November 1960. He became the first African American undergraduate student admitted and was one of three who enrolled in January 1961. </p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>Robinson went on to serve on Knoxville’s City Council—where he was the first African American representative elected in more than 50 years—and as vice president of economic development for the 1982 World’s Fair. He taught political science and served in purchasing and government relations at UT before being vice president of equity and diversity for the UT System in 2000, a role he held until his retirement in 2014. Robinson still gives occasional lectures and writes political opinion columns. </p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>Among numerous other honors, Robinson is a charter member of UT’s African American Hall of Fame and was named by <em>Metro Pulse</em> newspaper as one of the 100 most influential Knoxvillians of the 20th century. </p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p><a href="#">Read about the scholarship fund endowed by Robinson</a></p>
+<p>Aliquam sem et tortor consequat id porta nibh. Non diam phasellus vestibulum lorem sed risus ultricies tristique. Id neque aliquam vestibulum morbi blandit cursus risus at. Luctus accumsan tortor posuere ac. Enim ut tellus elementum sagittis vitae et leo duis ut. Orci nulla pellentesque dignissim enim. Augue mauris augue neque gravida in fermentum et sollicitudin. Integer eget aliquet nibh praesent tristique magna sit amet. Risus nec feugiat in fermentum posuere urna. Eget aliquet nibh praesent tristique magna sit amet purus gravida. Consectetur adipiscing elit pellentesque habitant morbi.</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"className":"bioCard"} -->
 <div class="wp-block-group bioCard"><!-- wp:group {"className":"bioHeadshot"} -->
-<div class="wp-block-group bioHeadshot"><!-- wp:image {"id":4872,"sizeSlug":"large","linkDestination":"none"} -->
-<figure class="wp-block-image size-large"><img src="https://images.utk.edu/wds/headshot-placeholder-1480x1920.png" alt="Headshot Placeholder 1480x1920" class="wp-image-4872"/></figure>
+<div class="wp-block-group bioHeadshot"><!-- wp:image {"id":4870,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://images.utk.edu/wds/headshot-placeholder-740x960.png" alt="Headshot Placeholder 740x960" class="wp-image-4870"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"className":"bioInfo"} -->
 <div class="wp-block-group bioInfo"><!-- wp:heading {"level":3} -->
-<h3>Scott Niswonger</h3>
+<h3>First Last</h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><em>Honorary Doctor of Humane Letters, Spring 2018</em></p>
+<p><em>Honorary Doctor of Specific Things, Spring 2022</em> </p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p>Scott Niswonger&nbsp;was born in Van Wert, Ohio, where he learned to fly, soloing on his 16th birthday. He has flown everything from J-3 Cubs to the Boeing 747, and he enjoys flying his Gulfstream IV and his aerobatic Navy T-34 trainer. After graduation from Purdue, Niswonger moved to Greeneville, Tennessee, as the corporate pilot for the president of the Magnavox Company. Niswonger co-founded Landair Services and later formed Forward Air Corporation. He is executive chairman of Landair Transport Inc. and chairman emeritus of Forward Air Corporation. He is a member of the board of directors of First Horizon National Corporation, the parent organization of First Tennessee Bank. He is chairman of Tennessee State University board of trustees and a trustee of Tusculum College. He is especially proud of the Niswonger Educational Foundation, which he established in 2001 to create opportunities for individual and regional growth through educational programs, scholarships, and other charitable activities. Niswonger is the lead benefactor for Niswonger Children’s Hospital, home to one of only eight St. Jude affiliate clinics in the world. </p>
+<p>Ut enim blandit volutpat maecenas volutpat blandit aliquam. Neque sodales ut etiam sit amet. Nunc lobortis mattis aliquam faucibus purus in massa tempor. Nisi lacus sed viverra tellus in hac habitasse platea. Eget aliquet nibh praesent tristique magna sit amet. Ante in nibh mauris cursus mattis. Egestas diam in arcu cursus euismod quis viverra nibh. Cras sed felis eget velit aliquet sagittis. Bibendum arcu vitae elementum curabitur vitae nunc sed velit dignissim. Nibh venenatis cras sed felis eget velit aliquet. Maecenas accumsan lacus vel facilisis volutpat. Egestas maecenas pharetra convallis posuere morbi. Vitae justo eget magna fermentum iaculis eu. Sed risus pretium quam vulputate dignissim suspendisse. Volutpat ac tincidunt vitae semper quis lectus nulla at. Facilisi cras fermentum odio eu feugiat pretium nibh ipsum. Tellus rutrum tellus pellentesque eu. Pellentesque sit amet porttitor eget dolor morbi.</p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p><a href="https://youtu.be/ylN-tk8b4B0">Watch an excerpt from Niswonger's commencement address</a></p>
+<p>Aliquam sem et tortor consequat id porta nibh. Non diam phasellus vestibulum lorem sed risus ultricies tristique. Id neque aliquam vestibulum morbi blandit cursus risus at. Luctus accumsan tortor posuere ac. Enim ut tellus elementum sagittis vitae et leo duis ut. Orci nulla pellentesque dignissim enim. Augue mauris augue neque gravida in fermentum et sollicitudin. Integer eget aliquet nibh praesent tristique magna sit amet. Risus nec feugiat in fermentum posuere urna. Eget aliquet nibh praesent tristique magna sit amet purus gravida. Consectetur adipiscing elit pellentesque habitant morbi.</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
-<!-- wp:spacer -->
-<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- wp:spacer {"height":"5vw"} -->
+<div style="height:5vw" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
