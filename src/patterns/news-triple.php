37f85a5d34f801633f5f3a41f45aeef089ff2c0f
Sandra Paduch <69162526+spworky@users.noreply.github.com>
2022-11-21 15:41:08 -0500

    rename news-triple and remove header

--- src/patterns/news-triple.php
+++ src/patterns/news-triple.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Title: News Triple with Header
- * Slug: wds-news/news-triple-with-header
+ * Title: News Triple
+ * Slug: wds-news/news-triple
  * Categories: wds-news
  * Viewport Width: 1500 
  */
@@ -12,10 +12,6 @@
 <!-- wp:spacer {"height":"10vw"} -->
 <div style="height:10vw" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
-
-<!-- wp:heading {"textAlign":"left","textColor":"smokeyX","className":"me-0 quietHeading"} -->
-<h2 class="has-text-align-left me-0 quietHeading has-smokey-x-color has-text-color">News</h2>
-<!-- /wp:heading -->
 
 <!-- wp:latest-posts {"categories":[{"id":5,"count":3,"description":"","link":"http://localhost:8888/utkwds-child/wp-child/category/featured/","name":"Featured","slug":"featured","taxonomy":"category","parent":0,"meta":[],"_links":{"self":[{"href":"http://localhost:8888/utkwds-child/wp-child/wp-json/wp/v2/categories/5"}],"collection":[{"href":"http://localhost:8888/utkwds-child/wp-child/wp-json/wp/v2/categories"}],"about":[{"href":"http://localhost:8888/utkwds-child/wp-child/wp-json/wp/v2/taxonomies/category"}],"wp:post_type":[{"href":"http://localhost:8888/utkwds-child/wp-child/wp-json/wp/v2/posts?categories=5"}],"curies":[{"name":"wp","href":"https://api.w.org/{rel}","templated":true}]}}],"postsToShow":3,"excerptLength":30,"displayPostDate":true,"postLayout":"grid","className":" chancellorFeaturePost "} /-->
 
